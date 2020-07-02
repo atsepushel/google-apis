6d72cef2a9ed2a6fd685fba30a951ea0f8e10325
Andrii <Andrii@MACHINE-IQOB8V8>
2020-07-02 11:34:26 +0300

    Fixes

--- js-redirect-params/redirectParams.js
+++ js-redirect-params/redirectParams.js
@@ -6,51 +6,54 @@ var redirectParams = function () {
     // get params string
     var query = parser.search.substring(1);
     
-    var ingoring = [
-      "mailto:",
-      "google"
-    ];
+    if (query) {
+
+        var ingoring = [
+          "mailto:",
+          "google"
+        ];
 
 
-    Array.prototype.forEach.call(document.querySelectorAll("a"), function(ref){
-        // get current href value
-        var old = ref.href;
-        
-        console.log("HOST url: " + ref.hostname);
-        // check for ignore
-        for (var i = 0; i < ingoring.length; i++) {
-            
-            var v = ref.hostname || old;
-            if (v.startsWith(ingoring[i]) ) {
-                return;
+        Array.prototype.forEach.call(document.querySelectorAll("a"), function(ref){
+            // get current href value
+            var old = ref.href;
+
+            console.log("HOST url: " + ref.hostname);
+            // check for ignore
+            for (var i = 0; i < ingoring.length; i++) {
+
+                var v = ref.hostname || old;
+                if (v.startsWith(ingoring[i]) ) {
+                    return;
+                }
             }
-        }
 
-        // @REMOVE IF NEED
-        console.log("OLD url: " + old);
+            // @REMOVE IF NEED
+            console.log("OLD url: " + old);
 
-        // clear last / if exist                
-        if (old.endsWith("/")) {
-            old = old.substring(0, old.length - 1);
-        }
+            // clear last / if exist                
+            if (old.endsWith("/")) {
+                old = old.substring(0, old.length - 1);
+            }
 
-        // we can have 3 types of queries:
-        // 1. http://localhost?    - add "params"
-        // 2. http://localhost?a=1 - add "& params"
-        // 3. http://localhost     - add "? params"
-        if (old.includes("?")) {
-            if (old.endsWith("?")) {
-                ref.href = old + query;
+            // we can have 3 types of queries:
+            // 1. http://localhost?    - add "params"
+            // 2. http://localhost?a=1 - add "& params"
+            // 3. http://localhost     - add "? params"
+            if (old.includes("?")) {
+                if (old.endsWith("?")) {
+                    ref.href = old + query;
+                } else {
+                    ref.href = old + "&" + query;
+                }
             } else {
-                ref.href = old + "&" + query;
+                ref.href = old + "?" + query;
             }
-        } else {
-            ref.href = old + "?" + query;
-        }
 
-        // @REMOVE IF NEED
-        console.log("NEW url: " + ref.href);
-    });
+            // @REMOVE IF NEED
+            console.log("NEW url: " + ref.href);
+        });
+    }
 
     return query;
 };
